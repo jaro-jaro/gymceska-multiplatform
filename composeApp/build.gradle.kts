fa70b33120ba54e092962e333d561e8e9a2401a1
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2024-10-21 19:41:18 +0200

    chore: Bump version to 2.5.0-rc.1
    Increased app version code to 107 and version name to 2.5.0-rc.1.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -14,8 +14,8 @@ plugins {
     alias(libs.plugins.buildkonfig)
 }
 
-val appVersionCode = 106
-val appVersionName = "2.5.0-pre.3"
+val appVersionCode = 107
+val appVersionName = "2.5.0-rc.1"
 
 kotlin {
     js(IR) {
