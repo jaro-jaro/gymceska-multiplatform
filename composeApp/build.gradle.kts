becca680edcbbb2d93ab6964b0e058bb9484ddbb
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2024-12-07 18:01:03 +0100

    2.6.0-alpha.2 (111)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -14,8 +14,8 @@ plugins {
     alias(libs.plugins.buildkonfig)
 }
 
-val appVersionCode = 110
-val appVersionName = "2.6.0-alpha.1"
+val appVersionCode = 111
+val appVersionName = "2.6.0-alpha.2"
 
 kotlin {
     js(IR) {
